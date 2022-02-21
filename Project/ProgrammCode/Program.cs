0dc22dd87df0eb9efa9fd324e3935c35978e07e2
Alex <alex.kolinson98@mail.ru>
2022-02-21 14:23:36 +0500

    Method for initial array added

---
+++ Project/ProgrammCode/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу, которая из имеющегося массива строк формируте массив из строк, длина которых меньше либо равна 3 символа 
+void PrintInitialArray(string[] array)
+{
+    Console.Write("Изначальный массив: ( ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write(")");
+}
